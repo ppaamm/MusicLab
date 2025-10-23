6b72e97d9a6c584a5f2f70ee2283f7df51a2db9e
Pierre-Alexandre Murena <61550406+ppaamm@users.noreply.github.com>
2025-10-23 14:04:13 +0200

    Adding half duration

--- sequencing/durations.py
+++ sequencing/durations.py
@@ -1,6 +1,9 @@
 def beats_to_ticks(beats: float, ppq: int) -> int:
     return max(1, int(round(beats * ppq)))
 
+def half(ppq: int) -> int:
+    return beats_to_ticks(2.0, ppq)
+
 def quarter(ppq: int) -> int:
     return beats_to_ticks(1.0, ppq)
 
